bd076a933817202625d9e743f3cfd7c4ba53c32f
MayconW <83735465+MayconW@users.noreply.github.com>
2023-05-06 00:49:15 -0300

    new file:   Piano/Piano.png 	modified:   Piano/css/style.css 	modified:   Piano/index.html 	modified:   Piano/js/script.js

--- Piano/js/script.js
+++ Piano/js/script.js
@@ -4,6 +4,7 @@ const blackKeys = document.querySelectorAll('.key.black');
 const checkbox = document.querySelector('.checkbox_keys');
 const switcher = document.querySelector('.switcher');
 
+
 const playNote = (note) => {
     console.log(note.getAttribute('data-note'));
     const data_note = note.getAttribute('data-note');
@@ -12,6 +13,12 @@ const playNote = (note) => {
     
 }
 
+const playNoteAuto = (note) => {
+    
+    const audio = new Audio (`../Piano/notes/${note}.wav`);
+    audio.play();
+    
+}
 
 const black_pressed = (key) =>{
 
@@ -74,36 +81,67 @@ document.addEventListener('keydown', e => {
    event.preventDefault(); 
     if(e.repeat) return;
     const key = e.key.toUpperCase();
-    const whiteKeyIndex = ['TAB','Q','W','E','R','T','Y', 'U', 'I', 'O', 'P', '[',']', '/'].indexOf(key);
-    const blackKeyIndex = ['1','2','3','4','5', '6', '7', '8', '9', 'L', 'Ç', "~"].indexOf(key);
-    console.log(key);
+    const whiteKeyIndex = ['TAB','A','S','D','F','G','H', 'J', 'K', 'L', 'Ç', ']', '8', '0'].indexOf(key);
+    const blackKeyIndex = ['Q','W','R','T','Y', 'I', 'O', '[', '7', '9'].indexOf(key);
+
     if(whiteKeyIndex > -1){
+        
         white_pressed(whiteKeys[whiteKeyIndex]);
-       // playNote (whiteKeys[whiteKeyIndex]);
+
         white_unpressed(whiteKeys[whiteKeyIndex]);
         
-
+        if(checkbox.checked){
+          
+        }
     } 
     if(blackKeyIndex > -1){
         black_pressed(blackKeys[blackKeyIndex]);
-        //  playNote (blackKeys[blackKeyIndex]);
+       
         black_unpressed(blackKeys[blackKeyIndex]);
 
     }  
 });
+const DEF_DELAY = 1000;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms || DEF_DELAY));
+  }
+  
 
 
-checkbox.addEventListener('change', ({target}) => {
+checkbox.addEventListener('change', async ({target}) => {
+    //console.log(target);
+    const music_auto = [13,12,10,8,5,8,5,8,5,8];
     //alert(target.checked);
     if(target.checked){
         switcher.classList.add('switcher--active');
-      playNote(2);
+        for(let j=0; j<2;j++){ 
+            for(let i = 0; i < 10; i++){
+                if(j==0){
+                playNoteAuto(music_auto[i]);}
+                else{
+                    playNoteAuto(music_auto[i]+12);
+                }
+                if(i<5){
+                 await sleep(400);
+                }else{
+                    await sleep(450);
+                }}
+               
+            }
+            
+       
     }
-    switcher.classList.remove('switcher--active');
-
-
-
+    else{
+        switcher.classList.remove('switcher--active');
+        return;
+    }
+    
+   
 
 });
+
+
+
 
 
